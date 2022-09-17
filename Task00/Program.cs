6423a50bb38d723f9f43cc53c176b627190b4d5c
stanislav <stanislavmuratov@yandex.ru>
2022-09-17 21:07:55 +0300

    Add interpolation to string

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,8 +1,7 @@
-﻿// Pow
+﻿// Square
 // 4 -> 16
 // -3 -> 9
 // -7 -> 49
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.Write("Квадрат числа: ");
-Console.Write(num*num);
+Console.Write($"Квадрат числа: {num * num}");
