f5f0cd90e7d1fdcde3cc47d5a454a5dc887e8abf
stanislav <stanislavmuratov@yandex.ru>
2022-09-17 22:19:13 +0300

    Add is square of number?

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Is square of number?
+int [][] numbers = {new int []{25, 5}, new int []{2, 10}, new int []{9, -3}, new int []{-3, 9}};
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i][0] == numbers[i][1]*numbers[i][1])
+    {
+        Console.Write("Yes");
+    }
+    else
+    {
+        Console.Write("No");
+    }
+    Console.WriteLine("");
+    
+}
