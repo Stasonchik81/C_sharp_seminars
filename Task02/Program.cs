032c04105509b934cac11972c2993b7b2cc16527
stanislav <stanislavmuratov@yandex.ru>
2022-09-17 22:49:11 +0300

    added check on existing day of week

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -2,4 +2,12 @@
 string [] days = new [] {"monday", "tuesday", "wensday", "thirsday", "friday", "satueday", "sunday"};
 Console.Write("Введите номер дня недели: ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Сегодня {days[num-1]}!");
+if (num>0&num<7)
+{
+    Console.WriteLine($"Сегодня {days[num-1]}!");
+}
+else
+{
+    Console.Write("Такого дня не существует");
+}
+
