8b630adf45674580d0504d94481143eef3187292
stanislav <stanislavmuratov@yandex.ru>
2022-09-17 22:38:58 +0300

    Add name of week day

---
+++ Task02/Program.cs
@@ -0,0 +1,5 @@
+﻿// name of week day
+string [] days = new [] {"monday", "tuesday", "wensday", "thirsday", "friday", "satueday", "sunday"};
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сегодня {days[num-1]}!");
