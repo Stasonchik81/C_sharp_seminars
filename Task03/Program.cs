b67480080071c59aa4bf1e0a524a6fee74dc87ea
stanislav <stanislavmuratov@yandex.ru>
2022-09-18 12:09:59 +0300

    Integer numbers from -N to N

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Integer numbers from -N to N
+Console.WriteLine("Введите целое чило: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = -num; i <= num; i++)
+{
+    Console.Write($"{i},");
+}
