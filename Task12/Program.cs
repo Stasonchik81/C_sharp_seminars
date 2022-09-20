04d85e6ce2031905b46ee36c33736d1868248067
stanislav <stanislavmuratov@yandex.ru>
2022-09-20 20:15:11 +0300

    Is multiple

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+Console.WriteLine("Введите два числа: ");
+int first = Convert.ToInt32(Console.ReadLine());
+int second = Convert.ToInt32(Console.ReadLine());
+
+bool isMultiple(int num1, int num2)
+{
+    return num1 % num2 == 0;
+}
+
+if (!isMultiple(first, second)) Console.WriteLine($"Не кратно, остаток - {first % second}");
+else Console.WriteLine("Кратно!");
