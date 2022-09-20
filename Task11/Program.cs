38fb3c90bca56caf9c07a601b1a8d0cfa00ba434
stanislav <stanislavmuratov@yandex.ru>
2022-09-20 19:26:03 +0300

    remove second digit

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+int rndNum = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерировано число - {rndNum}");
+
+int removeSecondDigit(int num)
+{
+    int first = num / 100;
+    int third = num % 10;
+    int result = first * 10 + third;
+    return result;
+}
+
+Console.WriteLine($"Результат - {removeSecondDigit(rndNum)}");
