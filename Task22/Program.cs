22e4ebcd0ee558da00e7f832d8e7a2b569c648c5
stanislav <stanislavmuratov@yandex.ru>
2022-09-29 15:03:38 +0300

    Save Task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,14 @@
 ﻿// List of square number
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void printListSquares(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        if(i == num)Console.Write($"{Math.Pow(i, 2)}");
+        else Console.Write($"{Math.Pow(i, 2)},");
+    }
+}
+
+printListSquares(number);
--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99]. Пример для массива из 5, а не 123 элементов. 
+// В своём решении сделайте для 123:
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] myArr = GetRandomArray(123, 1, 100);
+PrintArray(myArr);
+int x = CountNumbersBetween(myArr, 10, 99);
+Console.WriteLine($" => {x}");
+
+int CountNumbersBetween(int[] arr, int minSize, int maxSize)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > minSize && arr[i] < maxSize) count++;
+    }
+    return count;
+}
+
+int[] GetRandomArray(int size, int min, int max)
+{
+    Random rnd = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+    Console.Write(" ]");
+}
